f29214957a3b0cfcf3a7aec304ecbfdfbfa9ba00
Namnarak <namnarak@users.noreply.github.com>
2025-10-23 20:01:17 +0800

    fix: Unable to recognize some older versions of Forge mods

---
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/game/version/mod/meta/ForgeOldModMetadataList.kt
@@ -0,0 +1,7 @@
+package com.movtery.zalithlauncher.game.version.mod.meta
+
+// from https://github.com/HMCL-dev/HMCL/commit/2c428faa9540e8666ce3cec07ff284cb53eab34a
+class ForgeOldModMetadataList(
+    val modListVersion: Int?,
+    val modList: List<ForgeOldModMetadata>?
+)
